6724379f39c9586481444eedfc932bc2e585d4d2
tinathinhinene <tina.goblet@gmail.com>
2024-07-24 14:21:09 +0200

    t

---
+++ projetbtn.html/projetbtn.js
@@ -0,0 +1,49 @@
+let un=document.getElementById('btn1');
+un.addEventListener("click", function (){
+    document.getElementById("btnd").style.background="#34dada";
+})
+
+let de=document.getElementById('btn2');
+de.addEventListener("click", function (){
+    document.getElementById("btnd").style.background="#a811a8";
+})
+
+let tr=document.getElementById('btn3');
+tr.addEventListener("click", function (){
+    document.getElementById("btnd").style.background="#d5e407";
+})
+
+let qu=document.getElementById('btn4');
+qu.addEventListener("click", function (){
+    document.getElementById("btnd").style.background="#37a7a7";
+})
+
+let ci=document.getElementById('btn5');
+ci.addEventListener("click", function (){
+    document.getElementById("btnd").style.background="#44a72b";
+})
+
+let si=document.getElementById('btn6');
+si.addEventListener("click", function (){
+    document.getElementById("btnd").style.background="#1f29bb";
+})
+
+let se=document.getElementById('btn7');
+se.addEventListener("click", function (){
+    document.getElementById("btnd").style.background="#d978f1";
+})
+
+let hu=document.getElementById('btn8');
+hu.addEventListener("click", function (){
+    document.getElementById("btnd").style.background="rgb(233, 34, 34)";
+})
+
+let ne=document.getElementById('btn9');
+ne.addEventListener("click", function (){
+    document.getElementById("btnd").style.background="rgb(175, 169, 169)";
+})
+
+let di=document.getElementById('btn10');
+di.addEventListener("click", function (){
+    document.getElementById("btnd").style.background="#da6548";
+})
